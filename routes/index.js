acc2809528c535d1f3d00e66f93afe0b84cdbe63
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2024-06-13 16:32:27 -0500

    Testing

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // User Login Endpoint
 router.post(`/login`, async (req, res) => {
     console.log(req.body);
-    return res.send(req.body);
+    return res.redirect('https://www.youtube.com/watch?v=HeCZZ131-HQ');
     
 })
 
